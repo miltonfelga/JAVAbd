96c408a2eea60128b54f3735dc6be02de6d02505
milton felga <milton.felga@etec.sp.gov.br>
2019-11-26 22:11:41 -0300

    iniciando projeto adicionar bd no java

---
+++ src/projetojavadb/Conexao.java
@@ -0,0 +1,53 @@
+
+package projetojavadb;
+import java.sql.*;
+
+public class Conexao {
+    public static Connection abrirConexao(){
+        Connection con = null;
+        
+        try{
+                
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        
+        String url = "";
+        url += "jdbc:mysql://127.0.0.1/estacionamento?";
+        url += "user=root&password=";
+        
+        con = DriverManager.getConnection(url);
+        System.out.println("Conexão aberta.");
+        
+        }catch (SQLException e){
+        System.out.println(e.getMessage());
+        }
+        
+        catch (ClassNotFoundException e){
+        System.out.println(e.getMessage());
+        }
+        
+        catch (Exception e){
+        System.out.println(e.getMessage());
+        }
+        
+        return con;
+    }
+    
+    public static void fecharConexao(Connection con){
+    
+        try{
+        con.close();
+        System.out.println("Conexão fechada.");
+        
+        }
+        catch (SQLException e){
+        System.out.println(e.getMessage());
+        }
+        
+        catch (Exception e){
+        System.out.println(e.getMessage());
+        }
+    }
+}
+
+
+
---
+++ src/projetojavadb/ProjetoJavaDB.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetojavadb;
+
+import java.sql.*;
+
+public class ProjetoJavaDB {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
